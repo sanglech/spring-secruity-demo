f96a40f4b41ca2852a89d729b3f4b48a2c218278
Christian <christian@sanglech.com>
2021-04-20 15:29:23 +0900

    Added Jdbc authentication
    -Added mysql-properties
    - removed memory auth and changed to jdbc auth
    - updated demo app config to setup and use jdbc connection

--- src/main/java/com/christian/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/christian/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.christian.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,14 +16,26 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	//add ref to security data source
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		//use jdbc auth
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+		
+		/*
 		//add our uses for in memory auth
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"))
 		.withUser(users.username("mary").password("test123").roles("MANAGER","EMPLOYEE"))
 		.withUser(users.username("susan").password("test123").roles("ADMIN","EMPLOYEE"));
+		*/
 	}
 
 	@Override
